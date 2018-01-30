1ce3a5720aa9a196042a8aaf1962dc7e604cd29b
Zoltan Makadi <zltnmakadi@gmail.com>
2018-01-30 06:35:36 +0100

    feat(/layouts): separate layouts for each section

---
+++ assets/components/control_panel.js
@@ -0,0 +1,31 @@
+'use strict';
+
+import ajax from 'ajax';
+import render from 'render';
+
+
+function controlPanel() {
+  function prevTrack() {
+    console.log('1')
+  }
+
+  function nextTrack() {
+    console.log('1')
+  }
+
+  function currentTrack() {
+    console.log('1')
+  }
+
+  function renderPlaylists() {
+    console.log('1')  
+  }
+
+  function removeTracklist() {
+    console.log('1')  
+  }
+
+  function renderTracklist() {
+    console.log('1')  
+  }
+}
